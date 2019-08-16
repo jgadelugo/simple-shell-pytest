6f0e0c095f4b423df55ca72da69bf2c0eddfe1b3
william <818@holbertonschool.com>
2019-08-16 13:14:10 +0000

    [LOCAL][FEATURE] Add read from file for interactive tests

--- simple_shell_pytest.py
+++ simple_shell_pytest.py
@@ -1,6 +1,21 @@
 import subprocess
 import difflib
+import os
 
+
+def myexit(message, code=0):
+  "This exits with a custom error message and exit code"
+  print(message)
+  exit(code)
+
+def get_commands(filename):
+  "This gets the commands from a file for interactive mode testing"
+  if os.path.isfile(filename):
+    f = open(filename, 'r')
+    if f.mode == 'r':
+      return f.readlines()
+    return []
+  myexit('Error: Commands file \'' + filename + '\' does not exist.', 1)
 
 def open_interactive_test(ex):
   "This prepares two shell processes in interactive mode."
@@ -34,5 +49,15 @@ def test_interactive(tests, sh, hsh):
   print('sh exit code: ' + str(shret))
   print('simple-shell exit code: ' + str(hshret))
 
-#x,y = open_interactive_test("/bin/bash")
-#test_interactive(["ls\n", "cd ..\n", "ls\n"],x,y)
+def run_interactive(ex, commandfile):
+  "This runs an interactive test using the given executable and the given file with commands"
+  if os.path.exists(ex):
+    print("__________ Interactive Test __________")
+    print("")
+    x,y = open_interactive_test(ex)
+    test_interactive(get_commands(commandfile), x, y)
+    return
+  myexit("Error: Executable file " + ex + " not found.")
+
+
+run_interactive("/bin/bash","temptest")
